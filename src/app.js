1fc94726903209d4da8829df590d4d4b1d9af9c1
Pratik Mewara <00pratik00mewara@gmail.com>
2020-06-17 20:53:49 +0530

    Add features of css

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(express.static(publicDiresctoryPath))
 
 app.get('',(req,res)=>{
     res.render('index',{
-        title:'Weather App',
+        title:'Weather',
         name: 'Pratik Mewara'
         
     })
@@ -38,7 +38,7 @@ app.get('/about',(req,res)=>{
 app.get('/help',(req,res)=>{
     res.render('help',{
         title:'Help',
-        name:'Pratik'
+        name:'Pratik Mewara'
     })
 })
 
